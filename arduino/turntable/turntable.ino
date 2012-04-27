da1175aeb9c83d65e724d075888abc2caa06b434
Patrick Yeon <patrickyeon@gmail.com>
2012-04-27 13:03:14 -0400

    (poor) command reading, pot (angle) measurement

--- arduino/turntable/turntable.ino
+++ arduino/turntable/turntable.ino
@@ -1,4 +1,4 @@
-const int stepdelay = 10; // in ms
+const int stepdelay = 5; // in ms
 long laststep;
 int curstep;
 
@@ -13,26 +13,115 @@ const int phase3 = 9;
 #define OUTL(pin) pinMode(pin, OUTPUT); digitalWrite(pin, LOW);
 #define OUTH(pin) pinMode(pin, OUTPUT); digitalWrite(pin, HIGH);
 
+// potentiometer and the limit switches
+// pot is on an analog pin, the switches on dig. pins
+const int potpin = 5;
+// pot readings at +/- 90 degrees
+int neg90 = 207;
+int pos90 = 793;
+const int llimpin = 2;
+const int rlimpin = 4;
+unsigned int lastpos = 0;
+
 void setup(){
   OUTZ(phase1);
   OUTZ(phase2);
   OUTZ(phase3);
+
   laststep = millis();
   curstep = 0;
-  pinMode(gopin, INPUT);
-  digitalWrite(gopin, HIGH);
-  pinMode(dirpin, INPUT);
-  digitalWrite(dirpin, HIGH);
+
+  init_din(gopin);
+  init_din(dirpin);
+  init_din(llimpin);
+  init_din(rlimpin);
+  
+  // TODO tie A5 here to potpin
+  pinMode(A5, INPUT);
+  digitalWrite(A5, LOW);
+
+  Serial.begin(115200);
+  Serial.write("Alive.\n");
+}
+
+void init_din(int pin){
+    pinMode(pin, INPUT);
+    digitalWrite(pin, HIGH);
 }
 
 void loop(){
+  if(Serial.available() > 0){
+    char incmd[17] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+    int i = 0;
+    while((Serial.available() > 0) && (Serial.peek() != ';') && (i < 16)){
+      incmd[i++] = (char)Serial.read();
+      if(Serial.available() == 0)
+        delay(10);
+    }
+    if(Serial.peek() == ';'){
+      // if it's not, they sent us a command that's too long
+      Serial.read();
+      incmd[i] = '\0';
+      runcmd(incmd);
+    }
+    return;
+  }
+
   if(millis() - laststep < stepdelay)
     return;
   if(digitalRead(gopin) == HIGH)
     return;
-  
+
   curstep = (digitalRead(dirpin) == HIGH)? stepL(curstep) : stepR(curstep);
   laststep = millis();
+}
+
+void runcmd(char *cmd){
+  String cmdstr = String(cmd);
+  cmdstr.toUpperCase();
+  if(cmdstr.startsWith("CAL")){
+    calibrate();
+  }
+  else if(cmdstr.startsWith("GET")){
+    read_angle();
+  }
+  else if(cmdstr.startsWith("SET")){
+    set_angle(&cmdstr);
+  }
+  else{
+    ser_error(&cmdstr);
+  }
+}
+
+void calibrate(){
+  Serial.print("Start calibration.");
+}
+
+void read_angle(){
+  int ang = angle();
+  Serial.print("Angle: ");
+  Serial.println(ang);
+}
+
+void set_angle(String *cmd){
+  String moveto = (*cmd).substring((*cmd).indexOf(' ') + 1);
+  char buff[18];
+  (String("Moving to ") + moveto).toCharArray(buff, 18);
+  
+  Serial.println(buff);
+}
+
+void ser_error(String *cmd){
+  char bad_cmd[22 + 16];
+  (String("Unrecognized command: ") + (*cmd) + "\n").toCharArray(bad_cmd, 22 + 16);
+  Serial.print(bad_cmd);
+}
+
+
+
+int angle(){
+  int pot = analogRead(potpin);
+  return((((long)(pot - neg90) * 180) / (pos90 - neg90)) - 90);
 }
 
 int stepL(int from){
